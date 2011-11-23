d77e99f3f350c31eb7f0881f0e2b9960ab1414bc
Matthias Puech <puech@cs.unibo.it>
2011-11-24 00:42:57 +0100

    delete user create controller (no need anymore)

--- kalkuli/kalkuli.js
+++ kalkuli/kalkuli.js
@@ -2,11 +2,9 @@ steal(
 	'./kalkuli.css', 			// application CSS file
 	'./models/models.js',		// steals all your models
 	'./fixtures/fixtures.js',	// sets up fixtures for your models
-	'kalkuli/user/create',
 	'kalkuli/user/list',
 	function(){					// configure your application
 		
 	    $('#users').kalkuli_user_list();
-	    $('#create').kalkuli_user_create();
 	    $('#user_json').kalkuli_user_json();
 })
