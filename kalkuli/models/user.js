5b9d93c082e4684577266a16e195db127a0ad140
Florent Paillard <florent.paillard@gmail.com>
2011-11-23 23:38:48 +0100

    Implement user model create/update/destroy methods (for asynchrone server synchronisation and faster view refresh)

--- kalkuli/models/user.js
+++ kalkuli/models/user.js
@@ -11,9 +11,19 @@ $.Model('Kalkuli.Models.User',
 {
 	findAll: "/users.json",
   	findOne : "/users/{id}.json", 
-  	create : "/users.json",
- 	update : "/users/{id}.json",
-  	destroy : "/users/{id}.json"
+  	create : function(attrs, success, error) {
+		//attrs.id = Math.round(Math.random() * 1000000);
+		//$.ajax('/0987654321/person/create.json');
+		success && success(attrs);
+	},
+ 	update : function(id, params, success, error) {
+		//$.ajax('/0987654321/person/' + id + '/update.json');
+		success && success();
+	},
+  	destroy : function(id, success, error) {
+		//$.ajax('/0987654321/person/' + id + '/delete.json');
+		success && success();
+	}
 },
 /* @Prototype */
 {});
